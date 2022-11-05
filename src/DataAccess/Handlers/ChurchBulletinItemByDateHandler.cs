f78b0e9b90ffa8dfd98f4da8ee59b4b953062f33
mvcninja <mvcninja@texashomeresources.com>
2022-11-04 23:47:28 -0500

    Implement ChurchBulletinItemByDateHandler

--- src/DataAccess/Handlers/ChurchBulletinItemByDateHandler.cs
+++ src/DataAccess/Handlers/ChurchBulletinItemByDateHandler.cs
@@ -1,12 +1,22 @@
-﻿using CodingWithPalermo.ChurchBulletin.Core.Queries;
+﻿using CodingWithPalermo.ChurchBulletin.Core.Model;
+using CodingWithPalermo.ChurchBulletin.Core.Queries;
+using CodingWithPalermo.ChurchBulletin.DataAccess.Mappings;
 
 namespace CodingWithPalermo.ChurchBulletin.DataAccess.Handlers
 {
     public class ChurchBulletinItemByDateHandler
     {
-        public void Handle(ChurchBulletinItemByDateQuery query)
+        readonly DataContext _context;
+        
+        public ChurchBulletinItemByDateHandler(DataContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+
+        public IEnumerable<ChurchBulletinItem> Handle(ChurchBulletinItemByDateQuery query)
+        {
+            var results = _context.Set<ChurchBulletinItem>().Where(item => item.Date == query.TargetDate).AsEnumerable();
+            return results;
         }
     }
 }
--- src/IntegrationTests/ChurchBulletinItemByDateQueryTester.cs
+++ src/IntegrationTests/ChurchBulletinItemByDateQueryTester.cs
@@ -1,10 +1,18 @@
-﻿using CodingWithPalermo.ChurchBulletin.Core.Queries;
+﻿using CodingWithPalermo.ChurchBulletin.Core.Model;
+using CodingWithPalermo.ChurchBulletin.Core.Queries;
 using CodingWithPalermo.ChurchBulletin.DataAccess.Handlers;
+using CodingWithPalermo.ChurchBulletin.DataAccess.Mappings;
+using Shouldly;
 
 namespace CodingWithPalermo.ChurchBulletin.IntegrationTests
 {
     public class ChurchBulletinItemByDateQueryTester
     {
+        private static DataContext GetContext()
+        {
+            return new DataContext(new TestDataConfiguration());
+        }
+ 
         [SetUp]
         public void Setup()
         {
@@ -13,9 +21,41 @@ namespace CodingWithPalermo.ChurchBulletin.IntegrationTests
         [Test]
         public void ShouldGetWithinDate()
         {
-            var query = new ChurchBulletinItemByDateQuery(new DateTime(2000, 1, 1));
-            var handler = new ChurchBulletinItemByDateHandler();
-            handler.Handle(query);
+            var item1 = new ChurchBulletinItem { Date = new DateTime(2000, 1, 1) };
+            var item2 = new ChurchBulletinItem { Date = new DateTime(2000, 1, 2) };
+            var item3 = new ChurchBulletinItem { Date = new DateTime(1999, 12, 31) };
+            var item4 = new ChurchBulletinItem { Date = new DateTime(2000, 1, 1) };
+
+            using (var context = GetContext())
+            {
+                context.AddRange(item1, item2, item3, item4);
+                context.SaveChanges();
+            }
+
+            try
+            {
+                //arrange
+                var query = new ChurchBulletinItemByDateQuery(new DateTime(2000, 1, 1));
+                var handler = new ChurchBulletinItemByDateHandler(GetContext());
+
+                //act
+                var items = handler.Handle(query).ToArray();
+
+                //assert
+                items.Length.ShouldBe(2);
+                items.ShouldContain(item1);
+                items.ShouldContain(item4);
+                items.ShouldNotContain(item2);
+                items.ShouldNotContain(item3);
+            }
+            finally
+            {
+                using (var context = GetContext())
+                {
+                    context.RemoveRange(item1, item2, item3, item4);
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
